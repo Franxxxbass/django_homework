fbf4b2ddd3f4626a2a833301610c83c5731e44cb
Franxxxbass <63103120+Franxxxbass@users.noreply.github.com>
2022-03-22 19:24:44 +0100

    all functions

--- calculator/urls.py
+++ calculator/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from calculator import views
 
 urlpatterns = [
-    path('add/<int:first_num>/<int:second_num>/', views.add, name='add')
-    path('substract/<int:first_num>/<int:second_num>/', views.substract, name='substract')
+    path('add/<int:first_num>/<int:second_num>/', views.add, name='add'),
+    path('subtract/<int:first_num>/<int:second_num>/', views.subtract, name='subtract'),
+    path('multiply/<int:first_num>/<int:second_num>/', views.multiply, name='multiply'),
+    path('divide/<int:first_num>/<int:second_num>/', views.divide, name='divide'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -1,28 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def add(request, first_num, second_num):
-
     result = first_num + second_num
     return render(
         request,
         'calculator/add.html',
-        context = {
+        context={
             'first_num': first_num,
             'second_num': second_num,
             'result': result,
         }
     )
 
-def substract(request, first_num, second_num):
 
+def subtract(request, first_num, second_num):
     result = first_num - second_num
     return render(
         request,
-        'calculator/substract.html',
-        context = {
+        'calculator/subtract.html',
+        context={
             'first_num': first_num,
             'second_num': second_num,
             'result': result,
         }
     )
+
+
+def multiply(request, first_num, second_num):
+    result = first_num * second_num
+    return render(
+        request,
+        'calculator/multiply.html',
+        context={
+            'first_num': first_num,
+            'second_num': second_num,
+            'result': result,
+        }
+    )
+
+
+def divide(request, first_num, second_num):
+
+    result = first_num / second_num
+    return render(
+        request,
+        'calculator/divide.html',
+        context={
+            'first_num': first_num,
+            'second_num': second_num,
+            'result': result,
+            }
+        )
+
+
